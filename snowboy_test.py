17ed8d4d2e31597a36a1724a46a95893fad88ca1
Navbryce <plunkett.bryce.m@gmail.com>
2019-10-27 21:49:15 -0400

    started integrating snowboy. had to fix quite a few dependency issues with snowboy. interestingly, the snowboy terminate method hangs when called

---
+++ snowboy_test.py
@@ -0,0 +1,31 @@
+import snowboydecoder
+import os
+import sys
+import signal
+
+interrupted = False
+
+def detected():
+    print("detected")
+
+def signal_handler(signal, frame):
+    global interrupted
+    interrupted = True
+
+
+def interrupt_callback():
+    global interrupted
+    return interrupted
+
+
+# capture SIGINT signal, e.g., Ctrl+C
+signal.signal(signal.SIGINT, signal_handler)
+
+detector = snowboydecoder.HotwordDetector("Klauba.pmdl", sensitivity=0.5)
+print('Listening... Press Ctrl+C to exit')
+
+# main loop
+detector.start(detected_callback=detected,
+               interrupt_check=interrupt_callback,
+               sleep_time=0.03)
+
