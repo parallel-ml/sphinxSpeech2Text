e45f8a97ccb2003e42520c91ddf74a88bd6bff7b
Navbryce <plunkett.bryce.m@gmail.com>
2019-11-17 22:58:44 -0500

    redirected output ot python script

--- process-output.py
+++ process-output.py
@@ -1,7 +1,17 @@
 import sys
+from datetime import datetime
+
+def log_speech_input(speech_input):
+    with open("./output/log.txt", 'a') as f:
+        line = speech_input
+        time = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        line = time + ': ' + line
+        line = line.strip()
+        f.write(line + '\n')
 
 def process_speech_input(speech_input):
     print(speech_input)
+    log_speech_input(speech_input)
 
 if __name__ == "__main__":
     speech_input = sys.stdin.read()
