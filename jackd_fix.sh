1dafbd9034c860dce663e070694f033778009483
Ramyad Hadidi <ramyad.hadidi@gmail.com>
2019-12-02 17:49:25 -0500

    updated the jack fix command

--- jackd_fix.sh
+++ jackd_fix.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-
-pid=$(pgrep -xn pulseaudio)\
-	  && export DBUS_SESSION_BUS_ADDRESS="$(grep -ao -m1 -P '(?<=DBUS_SESSION_BUS_ADDRESS=).*?\0' /proc/"$pid"/environ)"
-
 jack_control start
 
